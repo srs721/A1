8f0ab834d9440ec28ee5513b732320f7f294884c
Srishti Jain <srshtjn@gmail.com>
2023-05-26 12:34:02 -0300

    A1

--- src/components/Products/ProductList.js
+++ src/components/Products/ProductList.js
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography,Rating,Card } from '@mui/material'
+import { Box, Grid, Typography,Rating} from '@mui/material'
 import React from 'react'
 import Button from '@mui/material/Button';
 
